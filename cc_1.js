ab4d7d98372cba2f2d67de2d3fc538d8f3108eb5
Roan Ali <alir13@usf.edu>
2024-08-29 16:04:08 -0400

    Task 1- Employee Information

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+// Task 1:Variables and Data Types
+let employeeName= "Cruella Deville";
+const employeeID= 1013;
+var isActive= true;
+
+console.log(employeeName);
+console.log(typeof(employeeName));
+console.log(employeeID);
+console.log(typeof(employeeID));
+console.log(isActive);
+console.log(typeof(isActive));
